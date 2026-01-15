57ac7acf1db2ab26971e2e2f94947b3e7eea32c3
bomboclatzak-sudo <bomboclatzak@gmail.com>
2026-01-16 08:55:14 +1100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1388,7 +1388,7 @@ local library library = {
 
         options = settings.new({
             text = "New Window",
-            size = Vector2.new(400, 200),
+            size = Vector2.new(450, 350),
             shadow = 10,
             transparency = 0.2,
             color = Color3.fromRGB(41, 74, 122),
